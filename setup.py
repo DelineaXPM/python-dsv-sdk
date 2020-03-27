23113118403234c8b724452ff0904facda2c9e18
Adam Migus <adam@migus.org>
2020-03-26 23:12:35 -0400

    Use the correct license.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",

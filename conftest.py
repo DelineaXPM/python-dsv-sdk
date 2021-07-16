230bdbf04505dc7f1518b72e1559fc2ea6d5f569
Ricky White <ricky@migusgroup.com>
2021-07-16 14:05:41 -0400

    Switched testing to use environment variables. Fixtures added for an authorizer and vault object.

--- conftest.py
+++ conftest.py
@@ -1,8 +1,29 @@
-import json
+import os
 import pytest
+from dotenv import load_dotenv
+from thycotic.secrets.vault import PasswordGrantAuthorizer, SecretsVault
+
+
+load_dotenv()
 
 
 @pytest.fixture
-def vault_json():
-    with open("test_vault.json") as f:
-        return json.load(f)
+def env_vars():
+    return {
+        "client_id": os.getenv("DSV_CLIENT_ID"),
+        "client_secret": os.getenv("DSV_CLIENT_SECRET"),
+        "tenant": os.getenv("DSV_TENANT"),
+    }
+
+
+@pytest.fixture
+def authorizer(env_vars):
+    return PasswordGrantAuthorizer(
+        env_vars['client_id'],
+        env_vars["client_secret"],
+        env_vars["tenant"],
+    )
+
+@pytest.fixture()
+def vault(authorizer):
+    return SecretsVault(authorizer)
